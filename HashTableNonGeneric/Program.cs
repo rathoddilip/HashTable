1dd08c8deae7bd472564abab978f2f94c16920e1
diliprathod32 <diliprathod32@gmail.com>
2021-03-02 23:21:18 +0530

    [Dilip] FindFrequencyOfWordInSentence)

--- HashTableNonGeneric/Program.cs
+++ HashTableNonGeneric/Program.cs
@@ -4,9 +4,29 @@ namespace HashTableNonGeneric
 {
     class Program
     {
+        public static void CountWordFrequency(string sentence, MyMapNode<string, int> frequencyDictionary)
+        {
+            string[] words = sentence.Split(' ');
+            foreach (string word in words)
+            {
+                if (frequencyDictionary.Exists(word))
+                {
+                    frequencyDictionary.Add(word, frequencyDictionary.Get(word) + 1);
+                }
+                else
+                {
+                    frequencyDictionary.Add(word, 1);
+                }
+            }
+            frequencyDictionary.PrintDictionary();
+            Console.WriteLine("---------------------------------");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to hash table collection");
+            MyMapNode<string, int> frequencyDictionary = new MyMapNode<string, int>(10);
+            CountWordFrequency("To be or not to be", frequencyDictionary);
+            Console.ReadLine();
         }
     }
 }
